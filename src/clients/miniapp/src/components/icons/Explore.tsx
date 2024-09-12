0c8375ad40b5f67b2769c1920986ba1e017a71b6
aybykovskii <aybykovskii@gmail.com>
2024-09-12 20:24:08 +0200

    Added app icons

---
+++ src/clients/miniapp/src/components/icons/Explore.tsx
@@ -0,0 +1,15 @@
+export const Explore = () => (
+	<svg
+		width={24}
+		height={24}
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		strokeLinecap="square"
+		strokeWidth={1}
+		fill="none"
+		color="currentColor"
+	>
+		<path d="M14.121 14.121 7.05 16.95l2.829-7.071L16.95 7.05z" />
+		<circle cx={12} cy={12} r={10} />
+	</svg>
+)
---
+++ src/clients/miniapp/src/components/icons/User.tsx
@@ -0,0 +1,17 @@
+export const User = () => (
+	<svg
+		width="24"
+		height="24"
+		fill="none"
+		viewBox="0 0 24 24"
+		strokeWidth={1}
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		stroke="currentColor"
+	>
+		<title>User</title>
+		<circle cx="12" cy="9.1" r="3" />
+		<circle cx="12" cy="12" r="10" />
+		<path d="M17 19.2c-.317-6.187-9.683-6.187-10 0" />
+	</svg>
+)
