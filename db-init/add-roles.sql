7b97ad10d9fa6537741dd06476d28162d049479e
aybykovskii <aybykovskii@gmail.com>
2024-12-20 16:05:58 +0500

    fixed db init script

--- db-init/add-roles.sql
+++ db-init/add-roles.sql
@@ -6,6 +6,12 @@ CREATE TABLE IF NOT EXISTS roles (
     "updatedAt" TIMESTAMP NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS "UserRoles" (
+    "userId" INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    "roleId" INTEGER NOT NULL REFERENCES roles(id) ON DELETE CASCADE,
+    PRIMARY KEY ("userId", "roleId")
+);
+
 INSERT INTO roles (id, type, description, "createdAt", "updatedAt") VALUES
     (1, 'user', 'Пользователь', NOW(), NOW()),
     (2, 'staff', 'Персонал', NOW(), NOW()),
