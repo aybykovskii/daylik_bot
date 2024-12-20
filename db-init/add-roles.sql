66e180d66b0de662817608b1ff740223cb599453
aybykovskii <aybykovskii@gmail.com>
2024-12-20 15:57:00 +0500

    fixed init script

--- db-init/add-roles.sql
+++ db-init/add-roles.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS roles (
     id SERIAL PRIMARY KEY,
     type VARCHAR(255) NOT NULL,
     description VARCHAR(255),
-    createdAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP NOT NULL
+    "createdAt" TIMESTAMP NOT NULL,
+    "updatedAt" TIMESTAMP NOT NULL
 );
 
-INSERT INTO roles (id, type, description, createdAt, updatedAt) VALUES
+INSERT INTO roles (id, type, description, "createdAt", "updatedAt") VALUES
     (1, 'user', 'Пользователь', NOW(), NOW()),
     (2, 'staff', 'Персонал', NOW(), NOW()),
     (3, 'admin', 'Администратор', NOW(), NOW())
