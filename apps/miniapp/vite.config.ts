aa7712e0bbf2a28e614cda0f7b946bbf5916146d
aybykovskii <aybykovskii@gmail.com>
2025-04-25 14:30:10 +0800

    added env to miniapp

--- apps/miniapp/vite.config.ts
+++ apps/miniapp/vite.config.ts
@@ -1,30 +1,39 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import viteEnvironmentPlugin from 'vite-plugin-environment'
 
-export default defineConfig({
-  publicDir: './public',
-  plugins: [react(), viteEnvironmentPlugin('all')],
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
-      generateScopedName: '[local]_[hash:base64:5]',
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    port: Number.parseInt(process.env.MINI_APP_PORT!, 10),
-    host: true,
-    proxy: {
-      '/api': {
-        target: `http://server:${process.env.SERVER_PORT}`,
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  process.env = {
+    ...process.env,
+    ...env,
+  }
+
+  return {
+    publicDir: './public',
+    plugins: [react(), viteEnvironmentPlugin('all')],
+    css: {
+      modules: {
+        localsConvention: 'camelCase',
+        generateScopedName: '[local]_[hash:base64:5]',
       },
     },
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      port: Number.parseInt(env.MINI_APP_PORT!, 10),
+      host: true,
+      proxy: {
+        '/api': {
+          target: `http://server:${env.SERVER_PORT}`,
+          changeOrigin: true,
+        },
+      },
+    },
+  }
 })
